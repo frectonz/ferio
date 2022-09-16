3831078cd01a84c47a484e99f4dd7aaa628d13bb
frectonz <fraol0912@gmail.com>
2022-09-16 17:44:09 +0300

    change ferio-bot to ferio-api

---
+++ ferio-api/src/main.rs
@@ -0,0 +1,18 @@
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Listening on http://{}", addr.to_string());
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
